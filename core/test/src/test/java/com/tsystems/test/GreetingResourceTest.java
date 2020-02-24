1abe616069a201b8201ffc1252859bafc834dc15
vsnikalex <vsnikalex@gmail.com>
2020-02-24 19:12:16 +0300

    [core] Implement HTTP based Testing

--- core/test/src/test/java/com/tsystems/test/GreetingResourceTest.java
+++ core/test/src/test/java/com/tsystems/test/GreetingResourceTest.java
@@ -3,6 +3,8 @@ package com.tsystems.test;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -12,10 +14,21 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/greeting")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("hello " + uuid));
     }
 
 }
