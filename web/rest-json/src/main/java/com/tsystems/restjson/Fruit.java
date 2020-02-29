02182bf821e77aef8a5fa67eecb8bdd663935584
vsnikalex <vsnikalex@gmail.com>
2020-02-29 21:36:48 +0300

    [web] Generate code for "Writing REST JSON Services"

---
+++ web/rest-json/src/main/java/com/tsystems/restjson/Fruit.java
@@ -0,0 +1,15 @@
+package com.tsystems.restjson;
+
+public class Fruit {
+
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
