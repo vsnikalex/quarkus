242317941b2e49b8398fe2f78bbc0b0bc4ca2ab0
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-03-08 16:36:41 +0300

    [web] Implement manual validation, remove generated tests

--- web/validation/src/main/java/com/tsystems/validation/BookResource.java
+++ web/validation/src/main/java/com/tsystems/validation/BookResource.java
@@ -1,16 +1,60 @@
 package com.tsystems.validation;
 
-import javax.ws.rs.GET;
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Path("/books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public static class Result {
+
+        Result(String message) {
+            this.success = true;
+            this.message = message;
+        }
+
+        Result(Set<? extends ConstraintViolation<?>> violations) {
+            this.success = false;
+            this.message = violations.stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.joining(", "));
+        }
+
+        private String message;
+        private boolean success;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
     }
+
+    @Inject
+    Validator validator;
+
+    @Path("/manual-validation")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Result tryMeManualValidation(Book book) {
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        if (violations.isEmpty()) {
+            return new Result("Book is valid! It was validated by manual validation.");
+        } else {
+            return new Result(violations);
+        }
+    }
+
 }
