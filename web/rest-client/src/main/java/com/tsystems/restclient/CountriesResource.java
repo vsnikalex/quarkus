c88fc94533f1000db6e2b0333735c042407b5f9f
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-03-09 15:34:17 +0300

    [web] Implement REST Client and tests

--- web/rest-client/src/main/java/com/tsystems/restclient/CountriesResource.java
+++ web/rest-client/src/main/java/com/tsystems/restclient/CountriesResource.java
@@ -1,16 +1,34 @@
 package com.tsystems.restclient;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
 
 @Path("/country")
 public class CountriesResource {
 
+    @Inject
+    @RestClient
+    CountriesService countriesService;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Path("/name/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<Country> name(@PathParam String name) {
+        return countriesService.getByName(name);
+    }
+
+    @GET
+    @Path("/name-async/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CompletionStage<Set<Country>> nameAsync(@PathParam String name) {
+        return countriesService.getByNameAsync(name);
     }
 }
---
+++ web/rest-client/src/main/java/com/tsystems/restclient/CountriesService.java
@@ -0,0 +1,26 @@
+package com.tsystems.restclient;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
+
+@Path("/v2")
+@RegisterRestClient(configKey="country-api")
+public interface CountriesService {
+
+    @GET
+    @Path("/name/{name}")
+    @Produces("application/json")
+    Set<Country> getByName(@PathParam String name);
+
+    @GET
+    @Path("/name/{name}")
+    @Produces("application/json")
+    CompletionStage<Set<Country>> getByNameAsync(@PathParam String name);
+}
