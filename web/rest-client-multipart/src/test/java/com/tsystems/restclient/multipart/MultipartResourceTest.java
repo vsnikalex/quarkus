78f780fbcfc945dbbab8d2ac350019edd228eb57
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-03-09 15:43:28 +0300

    [web] Generate code for "Using the REST Client with Multipart"

---
+++ web/rest-client-multipart/src/test/java/com/tsystems/restclient/multipart/MultipartResourceTest.java
@@ -0,0 +1,14 @@
+package com.tsystems.restclient.multipart;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@QuarkusTest
+public class MultipartResourceTest {
+
+
+
+}
