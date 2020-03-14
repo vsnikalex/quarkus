90042142843ed9da5b9e3072ff8847bcedc89275
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-03-14 14:20:23 +0300

    [web] Implement JsonWebToken and Claim Injection

--- web/security-jwt/src/main/java/com/tsystems/jwt/TokenSecuredResource.java
+++ web/security-jwt/src/main/java/com/tsystems/jwt/TokenSecuredResource.java
@@ -1,11 +1,15 @@
 package com.tsystems.jwt;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Optional;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.json.JsonString;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,6 +17,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 /**
@@ -24,6 +30,10 @@ public class TokenSecuredResource {
 
     @Inject
     JsonWebToken jwt;
+
+    @Inject
+    @Claim(standard = Claims.birthdate)
+    Optional<JsonString> birthdate;
 
     @GET()
     @Path("permit-all")
@@ -47,4 +57,53 @@ public class TokenSecuredResource {
         String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJWT);
         return helloReply;
     }
+
+    @GET
+    @Path("winners")
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("Subscriber")
+    public String winners() {
+        int remaining = 6;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // If the JWT contains a birthdate claim, use the day of the month as a pick
+        if (jwt.containsClaim(Claims.birthdate.name())) {
+            String bdayString = jwt.getClaim(Claims.birthdate.name());
+            LocalDate bday = LocalDate.parse(bdayString);
+            numbers.add(bday.getDayOfMonth());
+            remaining --;
+        }
+        // Fill remaining picks with random numbers
+        while(remaining > 0) {
+            int pick = (int) Math.rint(64 * Math.random() + 1);
+            numbers.add(pick);
+            remaining --;
+        }
+        return numbers.toString();
+    }
+
+    @GET
+    @Path("winners2")
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("Subscriber")
+    public String winners2() {
+        int remaining = 6;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // If the JWT contains a birthdate claim, use the day of the month as a pick
+        if (birthdate.isPresent()) {
+            String bdayString = birthdate.get().getString();
+            LocalDate bday = LocalDate.parse(bdayString);
+            numbers.add(bday.getDayOfMonth());
+            remaining --;
+        }
+        // Fill remaining picks with random numbers
+        while(remaining > 0) {
+            int pick = (int) Math.rint(64 * Math.random() + 1);
+            numbers.add(pick);
+            remaining --;
+        }
+        return numbers.toString();
+    }
+
 }
