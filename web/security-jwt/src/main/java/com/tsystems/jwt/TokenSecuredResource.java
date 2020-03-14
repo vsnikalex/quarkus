603b4653fe0550a2e29477bead2cdefa82673055
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-03-14 13:03:23 +0300

    [web] Implement endpoint allowed for certain roles

--- web/security-jwt/src/main/java/com/tsystems/jwt/TokenSecuredResource.java
+++ web/security-jwt/src/main/java/com/tsystems/jwt/TokenSecuredResource.java
@@ -3,6 +3,7 @@ package com.tsystems.jwt;
 import java.security.Principal;
 
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -15,7 +16,7 @@ import javax.ws.rs.core.SecurityContext;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 /**
- * Version 1 of the TokenSecuredResource
+ * Version 2 of the TokenSecuredResource
  */
 @Path("/secured")
 @RequestScoped
@@ -29,6 +30,17 @@ public class TokenSecuredResource {
     @PermitAll
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@Context SecurityContext ctx) {
+        Principal caller =  ctx.getUserPrincipal();
+        String name = caller == null ? "anonymous" : caller.getName();
+        String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s", name, ctx.isSecure(), ctx.getAuthenticationScheme());
+        return helloReply;
+    }
+
+    @GET()
+    @Path("roles-allowed")
+    @RolesAllowed({"Echoer", "Subscriber"})
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloRolesAllowed(@Context SecurityContext ctx) {
         Principal caller =  ctx.getUserPrincipal();
         String name = caller == null ? "anonymous" : caller.getName();
         boolean hasJWT = jwt.getClaimNames() != null;
