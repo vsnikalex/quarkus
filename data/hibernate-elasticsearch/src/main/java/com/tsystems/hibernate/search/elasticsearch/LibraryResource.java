02fd6e73bed5248d03932d3aa2606ba92aa6e46d
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-03-22 15:31:34 +0300

    [data] Extend LibraryResource

--- data/hibernate-elasticsearch/src/main/java/com/tsystems/hibernate/search/elasticsearch/LibraryResource.java
+++ data/hibernate-elasticsearch/src/main/java/com/tsystems/hibernate/search/elasticsearch/LibraryResource.java
@@ -1,16 +1,86 @@
 package com.tsystems.hibernate.search.elasticsearch;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.tsystems.hibernate.search.elasticsearch.model.Author;
+import com.tsystems.hibernate.search.elasticsearch.model.Book;
+import org.jboss.resteasy.annotations.jaxrs.FormParam;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/library")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class LibraryResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Inject
+    EntityManager em;
+
+    @PUT
+    @Path("book")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void addBook(@FormParam String title, @FormParam Long authorId) {
+        Author author = Author.findById(authorId);
+        if (author == null) {
+            return;
+        }
+
+        Book book = new Book();
+        book.title = title;
+        book.author = author;
+        book.persist();
+
+        author.books.add(book);
+        author.persist();
+    }
+
+    @DELETE
+    @Path("book/{id}")
+    @Transactional
+    public void deleteBook(@PathParam Long id) {
+        Book book = Book.findById(id);
+        if (book != null) {
+            book.author.books.remove(book);
+            book.delete();
+        }
+    }
+
+    @PUT
+    @Path("author")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void addAuthor(@FormParam String firstName, @FormParam String lastName) {
+        Author author = new Author();
+        author.firstName = firstName;
+        author.lastName = lastName;
+        author.persist();
+    }
+
+    @POST
+    @Path("author/{id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void updateAuthor(@PathParam Long id, @FormParam String firstName, @FormParam String lastName) {
+        Author author = Author.findById(id);
+        if (author == null) {
+            return;
+        }
+        author.firstName = firstName;
+        author.lastName = lastName;
+        author.persist();
+    }
+
+    @DELETE
+    @Path("author/{id}")
+    @Transactional
+    public void deleteAuthor(@PathParam Long id) {
+        Author author = Author.findById(id);
+        if (author != null) {
+            author.delete();
+        }
     }
 }
