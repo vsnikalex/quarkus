2a716b7503b957b0c128979cfd05f8c445951705
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-03-28 11:55:32 +0300

    [data] Implement simple caching

---
+++ data/cache/src/main/java/com/tsystems/cache/WeatherForecastService.java
@@ -0,0 +1,36 @@
+package com.tsystems.cache;
+
+
+import io.quarkus.cache.CacheResult;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.time.LocalDate;
+
+@ApplicationScoped
+public class WeatherForecastService {
+
+    @CacheResult(cacheName = "weather-cache")
+    public String getDailyForecast(LocalDate date, String city) {
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return date.getDayOfWeek() + " will be " + getDailyResult(date.getDayOfMonth() % 4) + " in " + city;
+    }
+
+    private String getDailyResult(int dayOfMonthModuloFour) {
+        switch (dayOfMonthModuloFour) {
+            case 0:
+                return "sunny";
+            case 1:
+                return "cloudy";
+            case 2:
+                return "chilly";
+            case 3:
+                return "rainy";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+}
