f6b5ded12ca60ad73c31256fcb48c2d40a9b63c2
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-11 13:12:18 +0300

    [messaging] Implement AMQP Reactive Messaging, imperative usage

--- messaging/amqp/src/main/java/com/tsystems/amqp/PriceResource.java
+++ messaging/amqp/src/main/java/com/tsystems/amqp/PriceResource.java
@@ -1,16 +1,41 @@
 package com.tsystems.amqp;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import io.smallrye.reactive.messaging.annotations.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.reactivestreams.Publisher;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * A simple resource retrieving the "in-memory" "my-data-stream" and sending the items as server-sent events.
+ */
 @Path("/prices")
 public class PriceResource {
+
+    @Inject
+    @Channel("my-data-stream") Publisher<Double> prices;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @GET
+    @Path("/stream")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public Publisher<Double> stream() {
+        return prices;
+    }
+
+    @Inject @Channel("price-create")
+    Emitter<Double> priceEmitter;
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void addPrice(Double price) {
+        priceEmitter.send(price);
     }
 }
