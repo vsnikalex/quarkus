5efe6032f372a3e367a8049b85146d7166f3a4b8
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-11 10:53:54 +0300

    [data] Implement synchronous Dynamodb client

--- data/dynamodb/src/main/java/com/tsystems/dynamodb/FruitResource.java
+++ data/dynamodb/src/main/java/com/tsystems/dynamodb/FruitResource.java
@@ -1,16 +1,32 @@
 package com.tsystems.dynamodb;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/fruits")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class FruitResource {
 
+    @Inject
+    FruitSyncService service;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public List<Fruit> getAll() {
+        return service.findAll();
+    }
+
+    @GET
+    @Path("{name}")
+    public Fruit getSingle(@PathParam("name") String name) {
+        return service.get(name);
+    }
+
+    @POST
+    public List<Fruit> add(Fruit fruit) {
+        service.add(fruit);
+        return getAll();
     }
 }
