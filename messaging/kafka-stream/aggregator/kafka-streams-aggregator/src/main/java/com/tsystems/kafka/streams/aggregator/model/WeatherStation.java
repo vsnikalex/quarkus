a1fa9daf52998600793703bc9d41563a6b6d721a
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-12 11:30:30 +0300

    [messaging] Configure the aggregator

---
+++ messaging/kafka-stream/aggregator/kafka-streams-aggregator/src/main/java/com/tsystems/kafka/streams/aggregator/model/WeatherStation.java
@@ -0,0 +1,10 @@
+package com.tsystems.kafka.streams.aggregator.model;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class WeatherStation {
+
+    public int id;
+    public String name;
+}
