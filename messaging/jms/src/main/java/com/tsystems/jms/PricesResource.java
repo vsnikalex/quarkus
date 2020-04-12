837cc368ddf1bb30ebab26d11a59f4c8e34c9521
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-12 13:19:37 +0300

    [messaging] Implement JMS messaging

--- messaging/jms/src/main/java/com/tsystems/jms/PricesResource.java
+++ messaging/jms/src/main/java/com/tsystems/jms/PricesResource.java
@@ -1,16 +1,24 @@
 package com.tsystems.jms;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * A simple resource showing the last price.
+ */
 @Path("/prices")
 public class PricesResource {
 
+    @Inject
+    PriceConsumer consumer;
+
     @GET
+    @Path("last")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String last() {
+        return consumer.getLastPrice();
     }
 }
