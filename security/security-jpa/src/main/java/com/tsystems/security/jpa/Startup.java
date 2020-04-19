3f5f41677f8283706b596443fd69f0040d82a589
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-19 14:46:42 +0300

    [security] Configure JPA authentication

---
+++ security/security-jpa/src/main/java/com/tsystems/security/jpa/Startup.java
@@ -0,0 +1,19 @@
+package com.tsystems.security.jpa;
+
+import io.quarkus.runtime.StartupEvent;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Singleton;
+import javax.transaction.Transactional;
+
+
+@Singleton
+public class Startup {
+    @Transactional
+    public void loadUsers(@Observes StartupEvent evt) {
+        // reset and load all test users
+        User.deleteAll();
+        User.add("admin", "admin", "admin");
+        User.add("user", "user", "user");
+    }
+}
