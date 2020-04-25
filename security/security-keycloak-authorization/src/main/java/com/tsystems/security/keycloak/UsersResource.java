c23584dcb4beca458e574181b9f4574ae9859ed5
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-25 13:35:17 +0300

    [security] Add basic Keycloak implementation

--- security/security-keycloak-authorization/src/main/java/com/tsystems/security/keycloak/UsersResource.java
+++ security/security-keycloak-authorization/src/main/java/com/tsystems/security/keycloak/UsersResource.java
@@ -1,5 +1,9 @@
 package com.tsystems.security.keycloak;
 
+import io.quarkus.security.identity.SecurityIdentity;
+import org.jboss.resteasy.annotations.cache.NoCache;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +12,27 @@ import javax.ws.rs.core.MediaType;
 @Path("/api/users")
 public class UsersResource {
 
+    @Inject
+    SecurityIdentity identity;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Path("/me")
+    @Produces(MediaType.APPLICATION_JSON)
+    @NoCache
+    public User me() {
+        return new User(identity);
+    }
+
+    public static class User {
+
+        private final String userName;
+
+        User(SecurityIdentity identity) {
+            this.userName = identity.getPrincipal().getName();
+        }
+
+        public String getUserName() {
+            return userName;
+        }
     }
 }
