0f06fcf7e2784b2b57ab379548e10d3188f70fb9
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-26 14:02:41 +0300

    [security] Implement sing and verify

--- security/vault-transit/src/main/java/com/tsystems/TransitResource.java
+++ security/vault-transit/src/main/java/com/tsystems/TransitResource.java
@@ -7,7 +7,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 @Path("/transit")
@@ -28,5 +30,24 @@ public class TransitResource {
     @Path("/decrypt")
     public String decrypt(String cipherText) {
         return transitSecretEngine.decrypt("my-encryption-key", cipherText).asString();
+    }
+
+    @POST
+    @Path("/sign")
+    public String sign(String input) {
+        return transitSecretEngine.sign("my-sign-key", input);
+    }
+
+    @POST
+    @Path("/verify")
+    @Consumes(APPLICATION_JSON)
+    public Response verify(VerifyRequest request) {
+        transitSecretEngine.verifySignature("my-sign-key", request.signature, request.input);
+        return Response.accepted().build();
+    }
+
+    public static class VerifyRequest {
+        public String signature;
+        public String input;
     }
 }
