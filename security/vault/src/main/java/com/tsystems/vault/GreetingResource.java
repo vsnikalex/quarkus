e8ac61d90b995e60e136fa83f1fd2efaa98a2292
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-26 13:05:02 +0300

    [security] Create application with a secret configuration

--- security/vault/src/main/java/com/tsystems/vault/GreetingResource.java
+++ security/vault/src/main/java/com/tsystems/vault/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.tsystems.vault;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,19 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "a-private-key")
+    String privateKey;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @GET
+    @Path("/private-key")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String privateKey() {
+        return privateKey;
     }
 }
