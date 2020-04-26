4c79c76519a9f8f73ec398f11c1815d510e7ed40
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-04-26 13:10:52 +0300

    [security] Implement programmatic access to the KV secret engine

--- security/vault/src/main/java/com/tsystems/vault/GreetingResource.java
+++ security/vault/src/main/java/com/tsystems/vault/GreetingResource.java
@@ -1,9 +1,12 @@
 package com.tsystems.vault;
 
+import io.quarkus.vault.VaultKVSecretEngine;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,6 +15,9 @@ public class GreetingResource {
 
     @ConfigProperty(name = "a-private-key")
     String privateKey;
+
+    @Inject
+    VaultKVSecretEngine kvSecretEngine;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -24,5 +30,12 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String privateKey() {
         return privateKey;
+    }
+
+    @GET
+    @Path("/secrets/{vault-path}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getSecrets(@PathParam("vault-path") String vaultPath) {
+        return kvSecretEngine.readSecret("myapps/vault-quickstart/" + vaultPath).toString();
     }
 }
