aa20238a9a74d51290e1451d133785d188cc8509
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-05-01 12:21:23 +0300

    [business-automation] Determine domain: Lesson, Room, Timeslot

---
+++ business_automation/optaplanner/src/main/java/com/tsystems/domain/Room.java
@@ -0,0 +1,23 @@
+package com.tsystems.domain;
+
+public class Room {
+
+    private String name;
+
+    public Room() {
+    }
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
