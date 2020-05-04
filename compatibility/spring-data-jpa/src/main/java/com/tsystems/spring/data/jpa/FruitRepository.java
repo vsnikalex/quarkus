221c2006c5e860ec416100995e728f1cce4d9db9
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-05-04 13:57:08 +0300

    [compatibility] Implement a CRUD repository

---
+++ compatibility/spring-data-jpa/src/main/java/com/tsystems/spring/data/jpa/FruitRepository.java
@@ -0,0 +1,10 @@
+package com.tsystems.spring.data.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface FruitRepository extends CrudRepository<Fruit, Long> {
+
+    List<Fruit> findByColor(String color);
+}
