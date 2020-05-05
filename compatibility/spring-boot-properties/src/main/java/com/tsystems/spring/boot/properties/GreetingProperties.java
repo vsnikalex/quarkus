7d15321ed700005f229c339ea223329f0cbe90b0
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-05-05 11:22:33 +0300

    [compatibility] Add a default value

--- compatibility/spring-boot-properties/src/main/java/com/tsystems/spring/boot/properties/GreetingProperties.java
+++ compatibility/spring-boot-properties/src/main/java/com/tsystems/spring/boot/properties/GreetingProperties.java
@@ -6,4 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class GreetingProperties {
 
     public String text;
+
+    public String suffix = "!";
 }
--- compatibility/spring-boot-properties/src/main/java/com/tsystems/spring/boot/properties/GreetingResource.java
+++ compatibility/spring-boot-properties/src/main/java/com/tsystems/spring/boot/properties/GreetingResource.java
@@ -15,6 +15,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return properties.text;
+        return properties.text + properties.suffix;
     }
 }
