57edca531d6246219c0ae8debf3c75aa7a728b80
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-05-09 11:47:46 +0300

    [miscellaneous] Update resource and test

--- miscellaneous/quartz/src/main/java/com/tsystems/quartz/TaskResource.java
+++ miscellaneous/quartz/src/main/java/com/tsystems/quartz/TaskResource.java
@@ -1,16 +1,18 @@
 package com.tsystems.quartz;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/tasks")
+@Produces(MediaType.APPLICATION_JSON)
 public class TaskResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public List<Task> listAll() {
+        return Task.listAll();
     }
 }
--- miscellaneous/quartz/src/test/java/com/tsystems/quartz/TaskResourceTest.java
+++ miscellaneous/quartz/src/test/java/com/tsystems/quartz/TaskResourceTest.java
@@ -1,21 +1,23 @@
 package com.tsystems.quartz;
 
+
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 @QuarkusTest
 public class TaskResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void tasks() throws InterruptedException {
+        Thread.sleep(1000); // wait at least a second to have the first task created
         given()
-          .when().get("/tasks")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/tasks")
+                .then()
+                .statusCode(200)
+                .body("size()", is(greaterThanOrEqualTo(1)));
     }
-
 }
