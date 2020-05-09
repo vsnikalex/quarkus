02bef4d8f65dcac4fa9848c64c659f11e5ed9ffd
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-05-09 11:00:47 +0300

    [alternative-languages] Add a service

--- alternative_languages/rest-kotlin/src/main/kotlin/com/tsystems/GreetingResource.kt
+++ alternative_languages/rest-kotlin/src/main/kotlin/com/tsystems/GreetingResource.kt
@@ -1,14 +1,35 @@
 package com.tsystems
 
+import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.inject.Default
+import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-@Path("/greeting")
+@ApplicationScoped
+class GreetingService {
+
+    fun greeting(name: String): String {
+        return "hello $name"
+    }
+
+}
+
+@Path("/")
 class GreetingResource {
+
+    @Inject
+    @field: Default
+    lateinit var service: GreetingService
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = Greeting("hello")
+    @Path("/greeting/{name}")
+    fun greeting(@PathParam("name") name: String): String {
+        return service.greeting(name)
+    }
+
 }
