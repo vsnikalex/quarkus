7e3d4ca2af93b51204b0788b35ab62fa8c4d7c0c
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-05-11 14:27:43 +0300

    [miscellaneous] Configure async mail sending

--- miscellaneous/sending-email/src/main/java/com/tsystems/mail/MailResource.java
+++ miscellaneous/sending-email/src/main/java/com/tsystems/mail/MailResource.java
@@ -1,16 +1,26 @@
 package com.tsystems.mail;
 
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.reactive.ReactiveMailer;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.concurrent.CompletionStage;
 
 @Path("/mail")
 public class MailResource {
 
+    @Inject
+    ReactiveMailer reactiveMailer;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Path("/async")
+    public CompletionStage<Response> sendASimpleEmailAsync() {
+        return reactiveMailer.send(
+                Mail.withText("repnikum@gmail.com", "A reactive email from quarkus", "This is my body"))
+                .subscribeAsCompletionStage()
+                .thenApply(x -> Response.accepted().build());
     }
 }
