66c019e151630e47dd74da42cc9b930a8da7e7fe
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-06-12 12:26:00 +0300

    [extensions] Add BuildStep mapping /greeting to the servlet

--- extensions/quarkus-greeting/deployment/src/main/java/org/acme/quarkus/greeting/deployment/QuarkusGreetingProcessor.java
+++ extensions/quarkus-greeting/deployment/src/main/java/org/acme/quarkus/greeting/deployment/QuarkusGreetingProcessor.java
@@ -2,6 +2,8 @@ package org.acme.quarkus.greeting.deployment;
 
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.undertow.deployment.ServletBuildItem;
+import org.acme.quarkus.greeting.GreetingServlet;
 
 class QuarkusGreetingProcessor {
 
@@ -12,4 +14,9 @@ class QuarkusGreetingProcessor {
         return new FeatureBuildItem(FEATURE);
     }
 
+    @BuildStep
+    ServletBuildItem createServlet() {
+        return ServletBuildItem.builder("greeting", GreetingServlet.class.getName())
+                    .addMapping("/greeting").build();
+    }
 }
