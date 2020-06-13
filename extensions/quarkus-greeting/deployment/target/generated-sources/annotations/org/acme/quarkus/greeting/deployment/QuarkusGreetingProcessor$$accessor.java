597534d1d3d4f60546445aa5d1d458697552851f
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-06-13 11:35:32 +0300

    [cloud] Update Quarkus version for Kubernetes quickstart

--- extensions/quarkus-greeting/deployment/target/generated-sources/annotations/org/acme/quarkus/greeting/deployment/QuarkusGreetingProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package org.acme.quarkus.greeting.deployment;
-public final class QuarkusGreetingProcessor$$accessor {
-    private QuarkusGreetingProcessor$$accessor() {}
-    public static Object construct() {
-        return new QuarkusGreetingProcessor();
-    }
-}
