b1085cfb1f039de9d1107ae9061062c28d053fd9
vsnikalex <vasilii.nikolaev@icloud.com>
2020-11-06 23:37:54 +0300

    [mutiny] Emit items with multi, subscribe with Consumer

--- patterns/communication/mutiny/src/main/java/comn/telekom/mutiny/GettingStartedResource.java
+++ patterns/communication/mutiny/src/main/java/comn/telekom/mutiny/GettingStartedResource.java
@@ -1,5 +1,7 @@
 package comn.telekom.mutiny;
 
+import io.smallrye.mutiny.Multi;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +13,11 @@ public class GettingStartedResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        Multi.createFrom().items("hello", "world")
+                .onItem().transform(s -> s.toUpperCase() + " ")
+                .onCompletion().continueWith("!")
+                .subscribe().with(System.out::print);
+
         return "hello";
     }
 }
