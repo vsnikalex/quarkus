b875650574f0afa56175f2f61a9247b2fa7f1a66
vnikolae <Vasilii.Nikolaev@t-systems.com>
2020-11-14 12:46:42 +0300

    [design-state] Add Player controller methods

--- patterns/design/behavioral/state/src/main/java/com/telekom/design/behavioral/state/PlayerResource.java
+++ patterns/design/behavioral/state/src/main/java/com/telekom/design/behavioral/state/PlayerResource.java
@@ -1,5 +1,6 @@
 package com.telekom.design.behavioral.state;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +9,38 @@ import javax.ws.rs.core.MediaType;
 @Path("/player")
 public class PlayerResource {
 
+    private Player player;
+
+    @PostConstruct
+    void init() {
+        player = new Player();
+    }
+
     @GET
+    @Path("/play")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String play() {
+        return player.getState().onPlay();
+    }
+
+    @GET
+    @Path("/stop")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String stop() {
+        return player.getState().onLock();
+    }
+
+    @GET
+    @Path("/next")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String next() {
+        return player.getState().onNext();
+    }
+
+    @GET
+    @Path("/prev")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String prev() {
+        return player.getState().onPrevious();
     }
 }
